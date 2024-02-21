b160defafe8613511465517cf3cb9dac54cb8aff
Ola <129540485+Maladelfineka@users.noreply.github.com>
2024-02-21 18:04:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let root = document.querySelector('.root');
-console.log(root);
+
 var headers = new Headers();
 headers.append("X-CSCAPI-KEY", "a0o0bjBHUVdhWVcxVE5LcVNVUHloSmRJbkJrbFV6NDJEbGc4cjBuMQ==");
 
 function panstwa(r){
-    console.log(r);
+
     let temp = ''
     for(let i of r){
         temp+=`
@@ -13,7 +13,7 @@ function panstwa(r){
         </div>
         `;
     }
-    console.log(temp)
+
     root.innerHTML=temp
 }
 var requestOptions = {
@@ -25,7 +25,7 @@ var requestOptions = {
 fetch("https://api.countrystatecity.in/v1/countries", requestOptions)
 .then(response => response.json())
 .then(result => {
-    // console.log(result);
+
     panstwa(result);
 })
 .catch(error => console.log('error', error));
